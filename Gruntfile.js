f8abbe6c3eb095108c67749535456baf9b6c1938
andreasonny83 <andreasonny83@gmail.com>
2015-09-07 16:40:19 +0100

    no message

--- Gruntfile.js
+++ Gruntfile.js
@@ -249,7 +249,6 @@ module.exports = function(grunt) {
 	grunt.loadNpmTasks('grunt-contrib-clean');
 	grunt.loadNpmTasks('grunt-contrib-sass');
 	grunt.loadNpmTasks('grunt-contrib-watch');
-	grunt.loadNpmTasks('grunt-contrib-concat');
 	grunt.loadNpmTasks('grunt-contrib-jshint');
 	grunt.loadNpmTasks('grunt-contrib-copy');
 
